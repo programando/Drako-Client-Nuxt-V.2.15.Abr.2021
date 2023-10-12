c6fa63a5080c232b56a0e22b05358ed340ee24c9
programando <jhonjamesmg@hotmail.com>
2023-10-12 12:59:06 -0500

    proveedores

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,10 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
+ plugins: [
+
+     { src: '@/plugins/filters.js' },
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
